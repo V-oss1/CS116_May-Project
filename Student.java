6cb30511b762a91feaff0f1a515361fc6aa9881d
ParseSink <joelccampos26@gmail.com>
2026-04-02 16:12:56 -0500

    added Course.java, RegistrationClient.java, Resources/Planning/UCL CLass Diagrams.xlsx, Student.java and deleted main.java Added outline defined by UCL class diagrams, implemented Course and Student classes, and created RegistrationClient.

---
+++ Student.java
@@ -0,0 +1,18 @@
+public class Student {
+    private String name;
+    private String aNumber;
+    private String[] previousCourses;
+    private String[] registeredNextSemester; // max 5 courses 
+
+    public Student(String name, String aNumber, String[] previous) {
+        this.name = name;
+        this.aNumber = aNumber;
+        this.previousCourses = previous;
+        this.registeredNextSemester = new String[5]; // limit check needed
+    }
+
+    // Logic to verify prerequisites
+    public boolean hasTakenPrereqs(String[] requiredPrereqs) {
+        return true; 
+    }
+}
